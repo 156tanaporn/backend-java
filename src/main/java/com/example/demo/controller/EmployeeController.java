a8b488c01e33e917456b11d9adf78a55d88b925b
156tanaporn <6310210156@email.psu.ac.th>
2023-07-19 12:25:35 +0700

    creatproduct&productcontroller

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -4,22 +4,31 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.xml.crypto.Data;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitterReturnValueHandler;
 
 import com.example.demo.model.Employee;
+
+import jakarta.websocket.server.PathParam;
 
 @RestController
 public class EmployeeController {
 	
-	private List<Employee> data =  new ArrayList<Employee>();
+	private List<Employee> data =  new ArrayList<Employee>(); 
 	
 	@GetMapping("/employee")
 	public List<Employee> getEmployee() {
 		return data;
-	}
+	    }
+	
 	
 	@PostMapping("/employee")
 	public Employee addEmployee(@RequestBody Employee body) {
@@ -28,10 +37,57 @@ public class EmployeeController {
 			if (body.getEmployeeId() == data.get(i).getEmployeeId())
 				
             return null;
-		}
+	        }
+	
 		data.add(body);
 		return body;
 		
 	}
+	
+	@GetMapping("/employee/{employeeId}")
+	private Employee getEmployeeDetail(@PathVariable Integer employeeId) {
+		System.out.println("employeeid = "+employeeId);
+		
+			
+		for (int i = 0; i < data.size();i++) {
+			if (employeeId == data.get(i).getEmployeeId()) {
+					
+	        return data.get(i);
+		    }
+		}
+		
+		return null;
+	}
+	
+	@PutMapping("/employee/{employeeId}")
+	public Employee updateEmployee(@PathVariable Integer employeeId ,@RequestBody Employee body) {
+		
+		
+		for (int i = 0; i < data.size();i++) {
+			if (employeeId == data.get(i).getEmployeeId()) {
+					
+	        data.get(i).setFirstName(body.getFirstName());
+	        data.get(i).setLastName(body.getLastName());
+	        data.get(i).setSalary(body.getSalary());
+	        return data.get(i);
+		    }
+		}
+		
+		return null;
+	}
+	
+	@DeleteMapping("/employee/{employeeId}")
+	public String deleteEmployee(@PathVariable Integer employeeId) {
+		
+		for (int i = 0; i < data.size();i++) {
+			if (employeeId == data.get(i).getEmployeeId()) {
+				data.remove(i);
+				return "delete sucess";
+			}
+		}
+				
+		return "employee not found";
+	}
+
 
 }
---
+++ src/main/java/com/example/demo/model/Product.java
@@ -0,0 +1,46 @@
+package com.example.demo.model;
+
+public class Product {
+	
+	private Integer productId;
+	private String productName;
+	private Double productPrice;
+	private Integer productAmout;
+	private String productDetail;
+	
+	
+	public Integer getProductId() {
+		return productId;
+	}
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public Double getProductPrice() {
+		return productPrice;
+	}
+	public void setProductPrice(Double productPrice) {
+		this.productPrice = productPrice;
+	}
+	public Integer getProductAmout() {
+		return productAmout;
+	}
+	public void setProductAmout(Integer productAmout) {
+		this.productAmout = productAmout;
+	}
+	public String getProductDetail() {
+		return productDetail;
+	}
+	public void setProductDetail(String productDetail) {
+		this.productDetail = productDetail;
+	}
+	
+	
+	
+
+}
